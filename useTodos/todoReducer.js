4b73e72f4a5f8f93fba000ace837708d551e9106
Aved Music Group <andreshiguitaamg@gmail.com>
2025-08-13 00:31:23 -0500

    Added useTodos file

---
+++ useTodos/todoReducer.js
@@ -0,0 +1,25 @@
+export const todoReducer = (initialState = [], action) => {
+    switch (action.type) {
+        case "ADD_TODO":
+            return [...initialState,
+                action.payload
+            ];
+
+        case "REMOVE_TODO":
+            return initialState.filter((task)=> task.id !== action.payload);
+        
+        case "TOGGLE_TODO":
+            return initialState.map(todo => {
+                if (todo.id === action.payload) {
+                    return {
+                        ...todo,
+                        done: !todo.done,
+                    }
+                }
+                return todo;
+            })
+    
+        default:
+            return initialState;
+    }
+}
